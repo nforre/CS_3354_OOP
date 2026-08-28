b954e5c181711593bc3e67944efb908ce102d5bf
nforre <journey.forrest@icloud.com>
2026-08-28 12:22:06 -0500

    Restart Triangle logic

--- TriangleProblem.java
+++ TriangleProblem.java
@@ -8,54 +8,9 @@ public class TriangleProblem {
 		Scanner keyboard = new Scanner(System.in);
 		int size;
 
-		System.out.println("Enter the size of the triangle [3-5]: "); // read size
+		System.out.print("Enter the size of the triangle [3-5]: "); // read size
 		size = keyboard.nextInt();
 
-		// draw top triangles
-		for (int row = 0; row < size; row++) {
-			//// white spaces
-			for (int column = 0; column < size - row - 1; column++) {
-				System.out.print(" ");
-			}
-			//// draw first triangle stars
-			for (int column = 0; column < row + 1; column++) {
-				System.out.print("*");
-			}
-			//// draw vertical gap
-			for (int column = 0; column < GAP_SIZE; column++) {
-				System.out.print(" ");
-			}
-			//// draw second triangle stars
-			for (int column = 0; column < row + 1; column++) {
-				System.out.print("*");
-			}
-			// move to next line
-			System.out.println();
-		}
-
-		// horizontal gap
-		for (int column = 0; column < GAP_SIZE / 2; column++) {
-			System.out.println();
-		}
 		
-		//  bottom triangles
-		for (int row = 0; row < size; row++) {
-			////  white spaces
-			for (int column = 0; column < row; column++) {
-				System.out.print(" ");
-			}
-			//// draw third triangle stars
-			for (int column = size; column > row; column--) {
-				System.out.print("*");
-			}
-			//// draw vertical gap
-			for (int column = 0; column < GAP_SIZE; column++) {
-				System.out.print(" ");
-			}
-			//// draw fourth triangle stars
-			for (int column = size; column > row; column--) {
-				System.out.print("*");
-			}
-        }    
     }
 }
