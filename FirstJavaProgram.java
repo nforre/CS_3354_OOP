f0b68adcd5b10e03b21bef46b345cff4f1cbc43a
nforre <journey.forrest@icloud.com>
2026-08-28 00:16:14 -0500

    Adds introductory Java program and class file
    Introduces a basic "hello world" style Java program, demonstrating console output.
    Also includes an empty class file, preparing for a future program.

---
+++ FirstJavaProgram.java
@@ -0,0 +1,11 @@
+//Journey Forrest, CS 3354, Fall 2026 
+
+class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hi, I am Journey Forrest.");
+        System.out.println("My First Java Program.");
+        System.out.print("CS");
+        System.out.print(" 3354");
+        System.out.print(" Object-Oriented Design and Programming\n");
+    }
+}
