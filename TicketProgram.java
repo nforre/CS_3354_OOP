f8f349a5dfb57091242b532fb8036f50d5a8ffa0
nforre <journey.forrest@icloud.com>
2026-08-28 01:16:39 -0500

    Adds initial TicketProgram structure
    Initializes the `TicketProgram` class with a `main` method.
    Declares core variables such as `ticketPrice`, `numOfTickets`, and `totalCost` to set up the basic framework for ticket calculation.

---
+++ TicketProgram.java
@@ -0,0 +1,9 @@
+public class TicketProgram {
+    public static void main(String[] args) {
+        final int ticketPrice = 15;
+        int numOfTickets;
+        double totalCost;
+        
+
+    }
+}
