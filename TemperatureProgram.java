63c4b071edf6e670f11d4b9017e8de741daae674
nforre <journey.forrest@icloud.com>
2026-08-28 02:01:27 -0500

    Adds basic user input for Fahrenheit temperature
    Initializes the main program structure and includes the necessary `Scanner` utility.
    Prompts the user to enter a temperature in Fahrenheit and captures this input, establishing the foundation for further temperature processing.

---
+++ TemperatureProgram.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+    System.out.print("Enter temperature in Fahrenheit: ");
+    Scanner input = new Scanner(System.in);
+    int temp = input.nextInt();
+
+
+}
