b48c9cc298b9be633c62ff5cce6478ec1d6291fb
nforre <journey.forrest@icloud.com>
2026-08-28 12:48:29 -0500

    Fixes inverted triangle pattern and renames scanner

--- TriangleProblem.java
+++ TriangleProblem.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class TriangleProblem {
     public static void main(String[] args) {
         final int GAP_SIZE = 2;
-		Scanner keyboard = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int size;
 
 		System.out.print("Enter the size of the triangle [3-5]: "); // read size
-		size = keyboard.nextInt();
+		size = input.nextInt();
 
 		for (int row = 0; row < size; row++){
 			for (int col = 0; col < size - row - 1; col++)
@@ -30,7 +30,7 @@ public class TriangleProblem {
 		for (int row = 0; row < size; row++){
 			for (int col = 0; col <row; col++)
 				System.out.print(" "); // white space
-			for (int col = size; col < row + 1; col++)
+			for (int col = size; col > row; col--)
 				System.out.print("*");
 
 			for (int col = 0; col < GAP_SIZE; col++)
