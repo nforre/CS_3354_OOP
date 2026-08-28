62c00b13243be1da7b809934486799cbd60dce03
nforre <journey.forrest@icloud.com>
2026-08-28 01:25:21 -0500

    Adds ticket purchase logic with user input
    Updates the TicketProgram to interactively prompt the user for the number of tickets they wish to purchase. It calculates the total cost based on a fixed price and displays the result to the console.

--- TicketProgram.java
+++ TicketProgram.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class TicketProgram {
     public static void main(String[] args) {
-        final int ticketPrice = 15;
+        final int ticketPrice = 15; // fixed ticket price
         int numOfTickets;
         double totalCost;
-        
+        Scanner input = new Scanner(System.in); // gets user input
+
+        System.out.print("Enter amount of tickets to purchase: ");
+        numOfTickets = input.nextInt();
+        totalCost = numOfTickets * ticketPrice;
+        System.out.println("Total cost of tickets: $" + totalCost);
 
     }
 }
