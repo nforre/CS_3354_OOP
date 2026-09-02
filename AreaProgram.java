3bf80bdd757f6c4eba9734f5664735316dc42aad
nforre <journey.forrest@icloud.com>
2026-09-02 10:12:15 -0500

    Adds initial structure for area calculator

---
+++ AreaProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AreaProgram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("=== AREA CALCULATOR ===\n");
+            
+        } while (option != 4);
+        input.close();
+    }
+}
