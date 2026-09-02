7f7463ea0d2a792d4120fc762a255d9f15cde594
nforre <journey.forrest@icloud.com>
2026-09-02 11:35:10 -0500

    Adds input validation for menu selection

--- AreaProgram.java
+++ AreaProgram.java
@@ -23,32 +23,34 @@ public class AreaProgram {
 		    System.out.println("4. Exit");
             System.out.print("Please select an option: ");
             option = input.nextInt();
-            
-            switch(option) {
-                case 1:
-                    System.out.println("\n\n=== AREA OF A SQUARE ===\n");
-                    System.out.print("Please enter a side length: ");
-                    int side = input.nextInt();
-                    area = side * side;
-                    System.out.println("The area of this square is " + area);
-                    break; 
-                case 2:
-                    System.out.println("\n\n=== AREA OF A CIRCLE ===\n");
-                    System.out.print("Please enter a radius: ");
-                    int radius = input.nextInt();
-                    area = Math.PI * Math.pow(radius, 2);
-                    System.out.println("The area of this circle is " + area);
-                    break; 
-                case 3:
-                    System.out.println("\n\n=== AREA OF A TRIANGLE ===\n");
-                    System.out.print("Please enter a base: ");
-                    int base = input.nextInt();
-                    System.out.print("Please enter a width height: ");
-                    int height = input.nextInt();
-                    area = base * height * .5;
-                    System.out.println("The area of this triangle is " + area);
-                    break; 
-            }
+            if (option < 1 || option > 4)
+                System.out.println("ERROR: Please provide a option between 1-4! ");
+            else
+                switch(option) {
+                    case 1:
+                        System.out.println("\n\n=== AREA OF A SQUARE ===\n");
+                        System.out.print("Please enter a side length: ");
+                        int side = input.nextInt();
+                        area = side * side;
+                        System.out.println("The area of this square is " + area);
+                        break; 
+                    case 2:
+                        System.out.println("\n\n=== AREA OF A CIRCLE ===\n");
+                        System.out.print("Please enter a radius: ");
+                        int radius = input.nextInt();
+                        area = Math.PI * Math.pow(radius, 2);
+                        System.out.println("The area of this circle is " + area);
+                        break; 
+                    case 3:
+                        System.out.println("\n\n=== AREA OF A TRIANGLE ===\n");
+                        System.out.print("Please enter a base: ");
+                        int base = input.nextInt();
+                        System.out.print("Please enter a width height: ");
+                        int height = input.nextInt();
+                        area = base * height * .5;
+                        System.out.println("The area of this triangle is " + area);
+                        break; 
+                }
         } while (option != 4);
         System.out.println("Exiting... Goodbye!");
         input.close();
