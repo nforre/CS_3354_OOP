56359df3e16ad1ebee0af0d0a07086af2cfa7dfe
nforre <journey.forrest@icloud.com>
2026-09-02 10:38:22 -0500

    Adds interactive menu and square area calculation

--- AreaProgram.java
+++ AreaProgram.java
@@ -3,10 +3,35 @@ public class AreaProgram {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int option;
+        double area;
 
         do {
             System.out.println("=== AREA CALCULATOR ===\n");
-            
+            System.out.println("1. Area of a Square");
+		    System.out.println("2. Area of a Circle");
+		    System.out.println("3. Area of a Triangle");
+		    System.out.println("4. Exit");
+            System.out.print("Please select an option: ");
+            option = input.nextInt();
+
+            if (option == 1) {
+                System.out.println("\n\n=== AREA OF A SQUARE ===");
+                System.out.print("Please enter a side length: ");
+                int side = input.nextInt();
+                area = side * side;
+                System.out.println("The area of this square is " + area);
+                break;
+            }
+            else if (option == 2) {
+
+            }
+            else if (option == 3) {
+
+            }
+            else{
+                break;
+            }
+
         } while (option != 4);
         input.close();
     }
