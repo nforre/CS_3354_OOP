893e2d77fb7b2b77516ddad514dc3fb73cbd15e9
nforre <journey.forrest@icloud.com>
2026-09-06 14:55:32 -0500

    Implements interactive area calculator program
    Provides a menu-driven application for calculating the area of squares, circles, and triangles.
    Enables users to perform multiple area computations in a single session, improving usability.
    Includes basic input validation for menu selections.

---
+++ ImprovedAreaProgram.java
@@ -0,0 +1,63 @@
+// Journey Forrest, CS 3354, Fall 2026 
+import java.util.Scanner;
+/**
+ * ImprovedAreaProgram is the improved version 
+ * of an area calculator application for 
+ * 2D Squares, Circles, and Triangles
+ */
+public class ImprovedAreaProgram {
+    
+    static void calcSquare (Scanner input){
+        System.out.println("\n\n=== AREA OF A SQUARE ===\n");
+        System.out.print("Please enter a side length: ");
+        int side = input.nextInt();
+        double area = side * side;
+        System.out.println("The area of this square is " + area);
+    }
+    /**
+	 * Executes all of ImprovedAreaProgram
+	 * 
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int option;
+        double area;
+
+        do {
+            System.out.println("\n=== AREA CALCULATOR ===\n");
+            System.out.println("1. Area of a Square");
+		    System.out.println("2. Area of a Circle");
+		    System.out.println("3. Area of a Triangle");
+		    System.out.println("4. Exit");
+            System.out.print("Please select an option: ");
+            option = input.nextInt();
+            if (option < 1 || option > 4)
+                System.out.println("ERROR: Please provide a option between 1-4! ");
+            else
+                switch(option) {
+                    case 1:
+                        calcSquare(input);
+                        break; 
+                    case 2:
+                        System.out.println("\n\n=== AREA OF A CIRCLE ===\n");
+                        System.out.print("Please enter a radius: ");
+                        int radius = input.nextInt();
+                        area = Math.PI * Math.pow(radius, 2);
+                        System.out.println("The area of this circle is " + area);
+                        break; 
+                    case 3:
+                        System.out.println("\n\n=== AREA OF A TRIANGLE ===\n");
+                        System.out.print("Please enter a base: ");
+                        int base = input.nextInt();
+                        System.out.print("Please enter a width height: ");
+                        int height = input.nextInt();
+                        area = base * height * .5;
+                        System.out.println("The area of this triangle is " + area);
+                        break; 
+                }
+        } while (option != 4);
+        System.out.println("Exiting... Goodbye!");
+        input.close();
+    }
+}
