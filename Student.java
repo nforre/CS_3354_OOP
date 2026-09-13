056d56a7a3cd87f3e6c39ee858f3e13050f9ef14
nforre <journey.forrest@icloud.com>
2026-09-13 16:54:27 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,34 +6,67 @@ public class Student {
     private String major;
     private double gpa;
 
+
+    /** 
+     * Sets the first name value of student class object
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /** 
+     * Returns the first name string of student class object
+     * @return String
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /** 
+     * Sets the last name value of student class object
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /** 
+     * Returns the last name string of student class object
+     * @return String
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /** 
+     * Sets the major value of student class object
+     * @param major
+     */
     public void setMajor(String major) {
         this.major = major;
     }
 
+    /** 
+     * Returns the major string of student class object
+     * @return String
+     */
     public String getMajor() {
         return major;
     }
 
+    /** 
+     * Sets the GPA value of student class object
+     * @param gpa
+     */
     public void setGpa(double gpa) {
         this.gpa = gpa;
     }
 
+    /** 
+     * Returns the GPA double of student class object
+     * @return double
+     */
     public double getGpa() {
         return gpa;
     }
@@ -51,19 +84,19 @@ public class Student {
 
     public static void main(String[] args) {
         Student student1 = new Student();
-        System.out.println("First name: " + student1.getFirstName());
+        System.out.println("\nFirst name: " + student1.getFirstName());
         System.out.println("Last name: " + student1.getLastName());
         System.out.println("Major: " + student1.getMajor());
         System.out.println("GPA: " + student1.getGpa());
         Student student2 = new Student("Peter","Parker", "CS", 3.5);
-        System.out.println("First name: " + student2.getFirstName());
+        System.out.println("\nFirst name: " + student2.getFirstName());
         System.out.println("Last name: " + student2.getLastName());
         System.out.println("Major: " + student2.getMajor());
         System.out.println("GPA: " + student2.getGpa());
         // Creating 3rd student
         Student student3 = new Student("Journey", "Forrest", "Art History", 3.6);
         student3.setMajor("Applied Math");
-        System.out.println("First name: " + student3.getFirstName());
+        System.out.println("\nFirst name: " + student3.getFirstName());
         System.out.println("Last name: " + student3.getLastName());
         System.out.println("Major: " + student3.getMajor());
         System.out.println("GPA: " + student3.getGpa());
