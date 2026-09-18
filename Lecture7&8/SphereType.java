323518bcf76a7d7fd40310f87cc9c54eb774feb5
nforre <journey.forrest@icloud.com>
2026-09-18 02:33:48 -0500

    Update SphereType.java

--- Lecture7&8/SphereType.java
+++ Lecture7&8/SphereType.java
@@ -7,4 +7,34 @@ public class SphereType extends CircleType {
     }
 
     @Override
+    public double getArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+    public SphereType() {
+        super(); // CircleType()
+    }
+
+    public SphereType(double radius) {
+        super(radius); // CircleType(double)
+    }
+
+    public static void main(String[] args) {
+        SphereType sphere = new SphereType(10);
+        System.out.println("Sphere's volume: " + sphere.getVolume());
+        System.out.println("Sphere's surface area: " + sphere.getArea());
+
+        CircleType circle = new CircleType(10);
+        System.out.println("Circles's areas: " + circle.getArea());
+        System.out.println("=========================");
+
+        displayArea(sphere);
+
+    }
+
+    public static void displayArea(CircleType circle) {
+        System.out.println("Printing area within a method.");
+        // circle.getVolume();
+        System.out.println("Area: " + circle.getArea());
+    }
 }
