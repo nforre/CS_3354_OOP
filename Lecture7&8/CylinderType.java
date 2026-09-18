a585740be698b950836630dbd072ff8b07d4b384
nforre <journey.forrest@icloud.com>
2026-09-18 02:24:32 -0500

    Implement Cylinder and Sphere volume methods

--- Lecture7&8/CylinderType.java
+++ Lecture7&8/CylinderType.java
@@ -1,5 +1,40 @@
 // Journey Forrest, CS 3354, Fall 2026 
 
-public class CylinderType {
-    
+public class CylinderType extends CircleType {
+    private double height;
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return super.getArea() * height;
+    }
+
+    // super.getArea (CircleType)
+    // this.getArea || getArea (CylinderType)
+
+    @Override 
+    public double getArea() {
+        return super.getArea() * 2 + height * Math.PI * radius * 2;
+    }
+
+    public CylinderType() {
+        super();
+    }
+
+    public CylinderType(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        CylinderType cylinder = new CylinderType(10,5);
+        System.out.println("Volume: " + cylinder.getVolume());
+        System.out.println("Surface area: " + cylinder.getArea());
+    }
 }
--- Lecture7&8/SphereType.java
+++ Lecture7&8/SphereType.java
@@ -1,5 +1,10 @@
 // Journey Forrest, CS 3354, Fall 2026 
 
-public class SphereType {
-    
+public class SphereType extends CircleType {
+
+    public double getVolume() {
+        return 4 / 3.0 * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
 }
