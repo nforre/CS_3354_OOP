321a10dd2360d300e394f709dd785a73988f8d0a
nforre <journey.forrest@icloud.com>
2026-09-17 22:29:05 -0500

    Update ArrayExample.java

--- Lecture7&8/ArrayExample.java
+++ Lecture7&8/ArrayExample.java
@@ -1,24 +1,35 @@
 // Journey Forrest, CS 3354, Fall 2026 
 
 public class ArrayExample {
-
-    class Weeble {
-        // This is an empty class
+    /**
+     * An empty nested class inside of the ArrayExample
+     * class. This type of array can only reserve slots for objects.
+     * We must use a for-loop to actually fill in the null
+     * slots with Weeble objects
+     * 
+     * FIX: Changed class to static so it could be declared 
+     * from the main function without an enclosing instance.
+     */
+    static class Weeble {
 
     }
 
-
     /** 
-     * Main function that creates arrays & checks if they're null
+     * Main function that fills in null slots in Weeble array
+     * & creates a int and String array (both null).
      * 
      * @param args
      */
     public static void main(String[] args) {
-        Weeble[] c = new Weeble[4];        // Size 4 array of Weeble objects
-        for (int i = 0; i < c.length; i++) // Loops through array c.length number of times
+        Weeble[] c = new Weeble[6];        // Size 6 array of Weeble references (MODIFIED: size from 4 to 6)
+        for (int i = 0; i < c.length; i++) // For-Loop to fill in null slots with actual Weeble objects
             if (c[i] == null)              // Checks if item in the array is null
                 c[i] = new Weeble();       // If item is null, item is assigned a new Weeble object
         int[] a = new int[20];             // Size 20 array of integer variables
-        String[] Array = new String[20];   // Size 20 array of String objects
+        String[] b = new String[20];   // Size 20 array of String objects (MODIFIED: name from "Array" to "b")
+        // Print statements for arrays
+        System.out.println("Array a holds " + a.length + " integer variables defaulted as 0.");
+        System.out.println("Array b holds " + b.length + " null String objects.");
+        System.out.println("Array c holds " + c.length + " initialized Weeble objects.");
     }
 }
