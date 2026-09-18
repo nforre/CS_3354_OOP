34aaa345abd2e43ebda33a6821443faf97364e07
nforre <journey.forrest@icloud.com>
2026-09-17 22:52:35 -0500

    Create ArrayListExample.java

---
+++ Lecture7&8/ArrayListExample.java
@@ -0,0 +1,15 @@
+// Journey Forrest, CS 3354, Fall 2026 
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> countries = new ArrayList<String>(20);
+        countries.add("Belgium");
+        countries.add("Italy");
+        countries.add("Thailand");
+
+        for (String s : countries) {
+            System.out.println(s);
+        }
+    }
+}
