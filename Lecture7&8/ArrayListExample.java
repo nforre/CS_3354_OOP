c1d5996edb1be13102c7fe7979d084e90eea5bd4
nforre <journey.forrest@icloud.com>
2026-09-17 23:20:48 -0500

    Update ArrayListExample.java

--- Lecture7&8/ArrayListExample.java
+++ Lecture7&8/ArrayListExample.java
@@ -5,19 +5,25 @@ public class ArrayListExample {
     /** 
      * Main function creates a size 20 String ArrayList 
      * for names of countries. Countries are added to list and 
-     * printed out using a for-loop.
+     * printed out using a for-loop. ArrayList allows for flexible 
+     * sizing.
+     * 
      * @param args
      */
     public static void main(String[] args) {
-        // Creates a String ArrayList with the initial capacity of 20
+        // Creates a String ArrayList with the initial capacity of 20.
         ArrayList<String> countries = new ArrayList<String>(20);
-        countries.add("Belgium");   // Adds country to ArrayList
-        countries.add("Italy");     // Adds country to ArrayList
-        countries.add("Thailand");  // Adds country to ArrayList
 
-        countries.add("United States"); // Adds additional country to ArrayList
-        countries.add("Japan");         // Adds additional country to ArrayList
-        countries.add("Canada");        // Adds additional country to ArrayList
+        // ArrayList differs from regular arrays by allowing elements to be
+        // added freely even past the initial capacity of 20.
+        countries.add("Belgium");   
+        countries.add("Italy");     
+        countries.add("Thailand");  
+
+        countries.add("United States"); // Testing ArrayList's flexible sizing 
+        countries.add("Japan");         
+        countries.add("Canada");
+        countries.add("Germany");        
 
         for (String s : countries) {      // For loop that prints out country strings
             System.out.println(s);        // countries printed in order added
