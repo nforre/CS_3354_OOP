d2a832cf9c575693c2230607929d5371e082fb01
nforre <journey.forrest@icloud.com>
2026-09-17 23:03:15 -0500

    Update ArrayListExample.java

--- Lecture7&8/ArrayListExample.java
+++ Lecture7&8/ArrayListExample.java
@@ -2,14 +2,25 @@
 import java.util.ArrayList;
 
 public class ArrayListExample {
+    /** 
+     * Main function creates a size 20 String ArrayList 
+     * for names of countries. Countries are added to list and 
+     * printed out using a for-loop.
+     * @param args
+     */
     public static void main(String[] args) {
+        // Creates a String ArrayList with the initial capacity of 20
         ArrayList<String> countries = new ArrayList<String>(20);
-        countries.add("Belgium");
-        countries.add("Italy");
-        countries.add("Thailand");
+        countries.add("Belgium");   // Adds country to ArrayList
+        countries.add("Italy");     // Adds country to ArrayList
+        countries.add("Thailand");  // Adds country to ArrayList
 
-        for (String s : countries) {
-            System.out.println(s);
+        countries.add("United States"); // Adds additional country to ArrayList
+        countries.add("Japan");         // Adds additional country to ArrayList
+        countries.add("Canada");        // Adds additional country to ArrayList
+
+        for (String s : countries) {      // For loop that prints out country strings
+            System.out.println(s);        // countries printed in order added
         }
     }
 }
